abe4f13d260eda38a617eec9dc97f4dc6d3d77b9
fucime <furone.antonio@gmail.com>
2015-09-27 14:32:59 +0200

    27 set 2015

--- cy-bss-core/DataBase/MySql/load_tb_bss_serv_01.sql
+++ cy-bss-core/DataBase/MySql/load_tb_bss_serv_01.sql
@@ -35,34 +35,3 @@ insert into BSST_BSO_SERVICE_OPERATION (BSV_N_SERVICE_ID,BSO_S_METHOD,BSO_S_NAME
 	
 	insert into BSST_BOP_OPERATION_PARAM(BOP_PARAM_NAME,BSO_N_OPERATION_ID,BOP_C_FLG_URL,BOP_C_REQUIRED,BOP_S_DESCRIPTION)
 		values ('OpID',4,'Y','Y','Operation Id');
-		
-delete from BSST_USR_USER;		
-delete from BSST_URO_ROLE order by URO_N_ROLE_ID desc;
-
-ALTER TABLE BSST_URO_ROLE AUTO_INCREMENT= 1;
-
-insert into BSST_URO_ROLE(URO_S_NAME,URO_S_DESCRIPTION,URO_N_PARENT_ROLE_ID)
-	values ('User', 'Simple User',null);
-insert into BSST_URO_ROLE(URO_S_NAME,URO_S_DESCRIPTION,URO_N_PARENT_ROLE_ID)
-	values ('Administrator', 'Administrator User',1);
-insert into BSST_URO_ROLE(URO_S_NAME,URO_S_DESCRIPTION,URO_N_PARENT_ROLE_ID)
-	values ('Super Administrator', 'Super Administrator User',2);
-insert into BSST_URO_ROLE(URO_S_NAME,URO_S_DESCRIPTION,URO_N_PARENT_ROLE_ID)
-	values ('Partner', 'Partner User',1);
-	
-	
-delete from BSST_LAN_LANGUAGE;
-
-ALTER TABLE BSST_LAN_LANGUAGE AUTO_INCREMENT= 1;
-
-insert into BSST_LAN_LANGUAGE(LAN_S_CODE,LAN_S_NAME)
-	values ('IT', 'Italiano');
-insert into BSST_LAN_LANGUAGE(LAN_S_CODE,LAN_S_NAME)
-	values ('EN', 'English');
-	
-
-ALTER TABLE BSST_USR_USER AUTO_INCREMENT= 1;
-	
-insert into BSST_USR_USER(USR_S_USER_ID,USR_S_PSW,USR_S_NAME,URO_N_ROLE_ID,LAN_N_LANG_ID)
-	values ('cybss','cybss','cybss',3,2);
-		
---
+++ cy-bss-core/DataBase/MySql/load_tb_user_02.sql
@@ -0,0 +1,31 @@
+		
+delete from BSST_USR_USER;		
+delete from BSST_URO_ROLE order by URO_N_ROLE_ID desc;
+
+ALTER TABLE BSST_URO_ROLE AUTO_INCREMENT= 1;
+
+insert into BSST_URO_ROLE(URO_S_NAME,URO_S_DESCRIPTION,URO_N_PARENT_ROLE_ID)
+	values ('User', 'Simple User',null);
+insert into BSST_URO_ROLE(URO_S_NAME,URO_S_DESCRIPTION,URO_N_PARENT_ROLE_ID)
+	values ('Administrator', 'Administrator User',1);
+insert into BSST_URO_ROLE(URO_S_NAME,URO_S_DESCRIPTION,URO_N_PARENT_ROLE_ID)
+	values ('Super Administrator', 'Super Administrator User',2);
+insert into BSST_URO_ROLE(URO_S_NAME,URO_S_DESCRIPTION,URO_N_PARENT_ROLE_ID)
+	values ('Partner', 'Partner User',1);
+	
+	
+delete from BSST_LAN_LANGUAGE;
+
+ALTER TABLE BSST_LAN_LANGUAGE AUTO_INCREMENT= 1;
+
+insert into BSST_LAN_LANGUAGE(LAN_S_CODE,LAN_S_NAME)
+	values ('IT', 'Italiano');
+insert into BSST_LAN_LANGUAGE(LAN_S_CODE,LAN_S_NAME)
+	values ('EN', 'English');
+	
+
+ALTER TABLE BSST_USR_USER AUTO_INCREMENT= 1;
+	
+insert into BSST_USR_USER(USR_S_USER_ID,USR_S_PSW,USR_S_NAME,URO_N_ROLE_ID,LAN_N_LANG_ID)
+	values ('cybss','cybss','cybss',3,2);
+		
